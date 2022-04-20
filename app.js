3da1b89464f235ca7a25cbec73427ef916b68729
daduque <daanduque@utp.edu.co>
2022-04-20 15:06:11 -0500

    with the login finish and some animations

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(3030, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.post('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
