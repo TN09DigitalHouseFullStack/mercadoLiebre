7517a1747c89c6f47030cb140f6612a6477cc86c
daanduqueutp <daanduque@utp.edu.co>
2022-04-20 15:26:06 -0500

    updating package.json with start script

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3030, ()=>{
-    console.log('Servidor funcionando');
+app.listen(process.env.PORT || 3030, ()=>{
+    console.log('Servidor funcionando puerto: ' + process.env.PORT);
 });
 
 app.get('/', (req,res)=>{
